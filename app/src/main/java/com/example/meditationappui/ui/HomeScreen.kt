09c3f43dde73495600d096af1ac97f6d083decfb
Gülten Doğan <63645518+gultendogan0@users.noreply.github.com>
2023-08-10 13:43:41 +0300

    added ChipSection function

--- app/src/main/java/com/example/meditationappui/ui/HomeScreen.kt
+++ app/src/main/java/com/example/meditationappui/ui/HomeScreen.kt
@@ -1,6 +1,7 @@
 package com.example.meditationappui.ui
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,17 +10,27 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.meditationappui.R
+import com.example.meditationappui.ui.theme.ButtonBlue
+import com.example.meditationappui.ui.theme.DarkerButtonBlue
 import com.example.meditationappui.ui.theme.DeepBlue
+import com.example.meditationappui.ui.theme.TextWhite
 
 @Composable
 fun HomeScreen(){
@@ -29,6 +40,7 @@ fun HomeScreen(){
     ){
         Column{
             GreetingSection()
+            ChipSection(chips = listOf("Sweet sleep","Insomnia","Depression"))
         }
     }
 }
@@ -63,5 +75,33 @@ fun GreetingSection(
             tint = Color.White,
             modifier = Modifier.size(24.dp)
         )
+    }
+}
+
+@Composable
+fun ChipSection(
+    chips: List<String>
+){
+    var selectedChipIndex by remember {
+        mutableStateOf(0)
+    }
+    LazyRow {
+        items(chips.size){
+            Box(contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .padding(start = 15.dp, top = 15.dp, bottom = 15.dp)
+                    .clickable {
+                        selectedChipIndex = it
+                    }
+                    .clip(RoundedCornerShape(10.dp))
+                    .background(
+                        if (selectedChipIndex == it) ButtonBlue
+                        else DarkerButtonBlue
+                    )
+                    .padding(15.dp)
+            ){
+                Text(text = chips[it], color = TextWhite)
+            }
+        }
     }
 }
